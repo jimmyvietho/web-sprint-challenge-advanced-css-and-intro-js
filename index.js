f49577368699264fec7c45c7469a77f386ea333f
Jimmy Ho <jimmyvietho@gmail.com>
2020-08-04 14:11:13 -0400

    answered interview questions and changed paths of html, js, and css

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ const artists = [
       "bio": "Hieronymus Bosch ( (listen); Dutch: [ɦijeːˈroːnimʏz ˈbɔs] (listen); born Jheronimus van Aken [jeːˈroːnimʏs fɑn ˈaːkə(n)]; c. 1450 – 9 August 1516) was a Dutch/Netherlandish draughtsman and painter from Brabant. He is one of the most notable representatives of the Early Netherlandish painting school. His work contains fantastic illustrations of religious concepts and narratives. Within his lifetime his work was collected in the Netherlands, Austria, and Spain, and widely copied, especially his macabre and nightmarish depictions of hell.",
       "wikipedia": "http://en.wikipedia.org/wiki/Hieronymus_Bosch",
       "paintings": 137
-    },
+    }, 
     {
       "id": 11,
       "name": "Kazimir Malevich",
@@ -208,11 +208,14 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+artists[8].name = "Vincent Van Gogh"
+console.log(artists[8].name)
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -222,18 +225,27 @@ const artists = [
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
+
 function getArtistByIndex(array, index) {
-    /* code here */
-  }
-  
+  return console.log(`The artist at index ${index} is ${array[index].name}`)
+}
+
+getArtistByIndex(artists, 0)
+
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
+function get20s(array){
 
-  /* Code here */
+    let newArtists = [];
+    for (i=0; i < array.length; i++) {
+      if (array[i].years.slice(0, 3).includes("19") == true ) {
+        newArtists.push(array[i].name);
+      }
+    };
+    console.log(newArtists);
 
 }
 
@@ -248,8 +260,9 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(array, index) {
+    array.splice(index, 1);
+    console.log(array.length)
   }
   
  
@@ -267,11 +280,23 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/* Code here */){
+function addArtist(id, name, years, nationality, bio){
 
-    /* Code here */
-
+  let obj = {
+    id: id,
+    name: name,
+    years: years,
+    nationality: nationality,
+    bio: bio,
   }
+  
+  artists.push(obj);
+  console.log(artists[id])
+  }
+
+addArtist(21, "Jimmy Ho", "1994-present", "Vietnamese", "Lorem Impsum")
+
+  
 
 /* Task 7: Create a function called lotsOfArt() that takes one argument: 
 
@@ -281,10 +306,15 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/* Code here */){
-
-  /* Code here */
-
+function lotsOfArt(array){
+    let newArr = [];
+    for (i=0; i<array.length; i++) {
+      if (array[i].paintings > 100) {
+        newArr.push(array[i].name)
+      }
+    ;
+  }
+console.log(newArr)
 }
 
 
